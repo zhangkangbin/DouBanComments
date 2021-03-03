e1765d9efd4c372c7f92a5803bb4aa2f67bce7d1
zhangkangbin <7066586+zhangkangbin@users.noreply.github.com>
2021-03-03 10:08:02 +0800

    Initial commit

---
+++ app/src/main/java/zkb/douban/MainActivity.kt
@@ -0,0 +1,85 @@
+package zkb.douban
+
+import android.accessibilityservice.AccessibilityServiceInfo
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
+import android.view.View
+import android.view.accessibility.AccessibilityManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity()  {
+    private var openBtn:Button?=null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initView()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if(isServiceEnabled()){
+            openBtn?.text = "服务已开启"
+        }
+    }
+    private fun  initView(){
+
+        openBtn=  findViewById<Button>(R.id.open)
+
+        if(isServiceEnabled()){
+            openBtn?.text = "服务已开启"
+        }
+        openBtn?.setOnClickListener {
+            val accessibleIntent = Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS)
+            startActivity(accessibleIntent)
+        }
+        val edtSecond=findViewById<EditText>(R.id.edtSecond)
+        val edtMsgCount=findViewById<EditText>(R.id.edtMsgCount)
+
+
+
+        val info=getSharedPreferences("info",Context.MODE_PRIVATE)
+        val edit = info.edit()
+        val sleepTime= info.getFloat("edtSecond",2f)
+        edtSecond.setText(sleepTime.toString())
+
+        val maxSendMessageCount=info.getInt("edtMsgCount",10)
+        edtMsgCount.setText(maxSendMessageCount.toString())
+
+        findViewById<View>(R.id.saveInfo).setOnClickListener {
+
+            if(edtSecond.text.isNullOrEmpty()) return@setOnClickListener
+            if(edtMsgCount.text.isNullOrEmpty()) return@setOnClickListener
+            edit.putFloat("edtSecond",edtSecond.text.toString().toFloat())
+            edit.putInt("edtMsgCount",edtMsgCount.text.toString().toInt())
+            edit.apply()
+
+            Toast.makeText(this,"保存成功！",Toast.LENGTH_SHORT).show()
+
+        }
+    }
+
+    /**
+     * 服务是否启用状态
+     *
+     * @return
+     */
+    private fun isServiceEnabled(): Boolean {
+        val accessibilityManager =  getSystemService(Context.ACCESSIBILITY_SERVICE) as AccessibilityManager;
+        val accessibilityServices: List<AccessibilityServiceInfo> =
+                accessibilityManager.getEnabledAccessibilityServiceList(AccessibilityServiceInfo.FEEDBACK_GENERIC)
+        for (info in accessibilityServices) {
+            // Log.d("kang",info.id)
+            if (info.id == "$packageName/.service.FireworksAccessibilityService") {
+                return true
+            }
+        }
+        return false
+    }
+}
---
+++ app/src/main/java/zkb/douban/service/CommentsContent.kt
@@ -0,0 +1,19 @@
+package zkb.douban.service;
+
+ class CommentsContent {
+
+     companion object{
+
+
+         fun getListText(){
+
+             val list= mutableListOf<String>()
+             list.add("房子几时有？把酒问青天。")
+             list.add("君家何处住？我想租福田。")
+             list.add("发帖暂借问，或恐是同乡")
+             list.add("家临鉴江水，同吃生蚝大")
+             list.add("大小不相识，有空回个帖")
+
+         }
+     }
+}
